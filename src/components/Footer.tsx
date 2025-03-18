480cfc6fe497f164ce36564b351c5c955cacb3ae
muhammed navas <muhammednavas6287@gmail.com>
2025-03-19 02:43:37 +0530

    solve the testimonia responsive issue

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,8 +11,8 @@ const Footer: React.FC = () => {
    ];
      
   return (
-    <footer className="bg-gradient-to-b from-[#2b3a8f] to-[#0c1129]  text-white py-12 px-6 md:px-12 lg:px-20">
-      <div className="max-w-7xl mx-auto">
+    <footer className="bg-gradient-to-b from-[#2b3a8f] to-[#0c1129] max-w-7xl mx-auto  text-white py-12 px-6 md:px-12 lg:px-20">
+      <div className="">
         <div className="flex flex-col lg:flex-row items-start justify-between w-full gap-8 mb-8">
           {/* Company Info */}
           <div className="lg:w-64 w-full">
@@ -41,7 +41,7 @@ const Footer: React.FC = () => {
               <h3 className="font-bold text-lg lg:text-xl mb-4">
                 Contact Details
               </h3>
-              <p className="mb-2">contact@infofin.com</p>
+              <p className="mb-2">Info@franchisemedia.in</p>
               <p className=" ">+919207887722</p>
               <p className=" ">+919207887722</p>
             </div>
